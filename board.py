3fc1166216b6cfa1ee5d1ba0fee8c321c7c9d916
Mathew <santanamathew@ufl.edu>
2024-12-02 23:06:55 -0500

    Implemented draw(self) for Cell class

--- board.py
+++ board.py
@@ -1,12 +1,14 @@
 import pygame
 
 class Cell:
-    def __init__(self, value, row, col, screen):
+    def __init__(self, value, row, col, screen, cell_size=60):
         self.value = value
         self.row = row
         self.col = col
         self.screen = screen
         self.sketched_value = 0
+        self.cell_size = cell_size
+        self.selected = False
 
 
     def set_cell_value(self, value):
@@ -18,7 +20,24 @@ class Cell:
 
 
     def draw(self):
-        pass
+        font = pygame.font.Font(None, 40)
+
+        if self.value != 0:
+            num_surf = font.render(str(self.value), 0, (66, 66, 66))
+
+            x_center = self. col * self.cell_size + self.cell_size // 2
+            y_center = self.row * self.cell_size + self.cell_size // 2
+
+            num_rect = num_surf.get_rect(center=(x_center, y_center))
+            self.screen.blit(num_surf, num_rect)
+        elif self.sketched_value != 0:
+            sketch_surf = font.render(str(self.sketched_value), 0, (150, 150, 150))
+            x_sketch_center = self.col * self.cell_size // 4
+            y_sketch_center = self.row * self.cell_size // 4
+            sketch_rect = sketch_surf.get_rect(center=(x_sketch_center, y_sketch_center))
+            self.screen.blit(sketch_surf, sketch_rect)
+
+        pygame.draw.rect(self.screen, (0, 0, 0),(self.col * self.cell_size, self.row * self.cell_size, self.cell_size, self.cell_size), 1)
 
 
 class Board:
