1c7f3e11228481eea45528eab4438c353a6cd964
Mathew <santanamathew@ufl.edu>
2024-12-03 17:05:00 -0500

    Updated main logic

--- sudoku.py
+++ sudoku.py
@@ -4,18 +4,34 @@ from sudoku_generator import generate_sudoku
 
 WIDTH = 638
 HEIGHT = 708
+BG_COLOR = (255, 255, 245)
+DIFFICULTY = "medium"
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sudoku")
 
-screen.fill((255, 255, 245))
+board = Board(9, 9, screen, DIFFICULTY)
+board.draw()
+
+
+def draw_game_over():
+    pass
+
 
 while True:
+    screen.fill(BG_COLOR)
+    board.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            clicked_cell = board.click(x, y)
+            if clicked_cell:
+                row, col = clicked_cell
+                board.select(row, col)
 
     pygame.display.update()
