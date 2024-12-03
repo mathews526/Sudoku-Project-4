15e2d587da995cd6cea8686e23f5e6183194d8c5
Mathew <santanamathew@ufl.edu>
2024-12-03 17:56:13 -0500

    Added key events

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,5 @@
 import pygame, sys
 from board import Board
-from sudoku_generator import generate_sudoku
 
 WIDTH = 638
 HEIGHT = 708
@@ -19,6 +18,18 @@ def draw_game_over():
     pass
 
 
+def key_events(key_event):
+    if board.selected_cell:
+        if key_event.key == pygame.K_RETURN:
+            if board.selected_cell.sketched_value != 0:
+                board.place_number(board.selected_cell.sketched_value)
+        elif pygame.K_1 <= key_event.key <= pygame.K_9:
+            sketch_value = key_event.key - pygame.K_0
+            board.sketch(sketch_value)
+        elif key_event.key == pygame.K_BACKSPACE:
+            board.clear()
+
+
 while True:
     screen.fill(BG_COLOR)
     board.draw()
@@ -27,11 +38,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             x, y = event.pos
             clicked_cell = board.click(x, y)
             if clicked_cell:
                 row, col = clicked_cell
                 board.select(row, col)
+        elif event.type == pygame.KEYDOWN:
+            key_events(event)
 
     pygame.display.update()
