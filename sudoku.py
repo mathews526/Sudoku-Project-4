e170eab7a1a70bfc5a1947ff040821d38f7f9f47
Mathew <santanamathew@ufl.edu>
2024-12-03 20:17:23 -0500

    Added main menu

--- sudoku.py
+++ sudoku.py
@@ -4,14 +4,62 @@ from board import Board
 WIDTH = 540
 HEIGHT = 640
 BG_COLOR = (255, 255, 245)
-DIFFICULTY = "medium"
+LINE_COLOR = (245, 152, 66)
 
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Sudoku")
 
-board = Board(WIDTH, 540, screen, DIFFICULTY)
-board.draw()
+def draw_game_start(screen):
+    start_title_font = pygame.font.Font(None, 100)
+    button_font = pygame.font.Font(None, 70)
+
+    # Color background
+    screen.fill(BG_COLOR)
+
+    # Initialize and draw title
+    title_surf = start_title_font.render("Sudoku", 0, LINE_COLOR)
+    title_rect = title_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 150))
+    screen.blit(title_surf, title_rect)
+
+    # Initialize text
+    easy_text = button_font.render("Easy", 0, (255, 255, 255))
+    medium_text = button_font.render("Medium", 0, (255, 255, 255))
+    hard_text = button_font.render("Hard", 0, (255, 255, 255))
+
+    # Initialize button background color and text
+    easy_surf = pygame.Surface((easy_text.get_size()[0] + 20, easy_text.get_size()[1] + 20))
+    easy_surf.fill(LINE_COLOR)
+    easy_surf.blit(easy_text, (10,10))
+    medium_surf = pygame.Surface((medium_text.get_size()[0] + 20, medium_text.get_size()[1] + 20))
+    medium_surf.fill(LINE_COLOR)
+    medium_surf.blit(medium_text, (10, 10))
+    hard_surf = pygame.Surface((hard_text.get_size()[0] + 20, hard_text.get_size()[1] + 20))
+    hard_surf.fill(LINE_COLOR)
+    hard_surf.blit(hard_text, (10, 10))
+
+    # Initialize button rectangle
+    easy_rect = easy_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 50))
+    medium_rect = medium_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 150))
+    hard_rect = hard_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 250))
+
+    # Draw buttons
+    screen.blit(easy_surf, easy_rect)
+    screen.blit(medium_surf, medium_rect)
+    screen.blit(hard_surf, hard_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if easy_rect.collidepoint(event.pos):
+                    difficulty = "easy"
+                    return difficulty
+                elif medium_rect.collidepoint(event.pos):
+                    difficulty = "medium"
+                    return difficulty
+                elif hard_rect.collidepoint(event.pos):
+                    difficulty = "hard"
+                    return difficulty
+        pygame.display.update()
 
 
 def draw_game_over():
@@ -29,22 +77,33 @@ def key_events(key_event):
         elif key_event.key == pygame.K_BACKSPACE:
             board.clear()
 
+if __name__ == '__main__':
 
-while True:
-    screen.fill(BG_COLOR)
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Sudoku")
+
+    difficulty = draw_game_start(screen) # Calls function to draw start screen
+
+    board = Board(WIDTH, 540, screen, difficulty)
     board.draw()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos
-            clicked_cell = board.click(x, y)
-            if clicked_cell:
-                row, col = clicked_cell
-                board.select(row, col)
-        elif event.type == pygame.KEYDOWN:
-            key_events(event)
 
-    pygame.display.update()
+    while True:
+        screen.fill(BG_COLOR)
+        board.draw()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                clicked_cell = board.click(x, y)
+                if clicked_cell:
+                    row, col = clicked_cell
+                    board.select(row, col)
+            elif event.type == pygame.KEYDOWN:
+                key_events(event)
+
+        pygame.display.update()
