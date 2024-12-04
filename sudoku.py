130643af9d8fcf960a8779a5114b593926604d17
Mathew <santanamathew@ufl.edu>
2024-12-04 13:18:44 -0500

    Added more comments to sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -8,6 +8,8 @@ LINE_COLOR = (245, 152, 66)
 
 
 def draw_game_start(screen):
+    # Start menu screen made with the help of the lecture videos from module 9 with the Tic Tac Toe example
+
     start_title_font = pygame.font.Font(None, 100)
     button_font = pygame.font.Font(None, 70)
 
@@ -46,35 +48,35 @@ def draw_game_start(screen):
     screen.blit(hard_surf, hard_rect)
 
 
-    while True:
+    while True: # Keeps user in the main menu until a button is pressed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if easy_rect.collidepoint(event.pos):
+                if easy_rect.collidepoint(event.pos): # If easy button pressed
                     difficulty = "easy"
-                    return difficulty
-                elif medium_rect.collidepoint(event.pos):
+                    return difficulty # Returns easy difficulty to main
+                elif medium_rect.collidepoint(event.pos): # If medium button pressed
                     difficulty = "medium"
-                    return difficulty
-                elif hard_rect.collidepoint(event.pos):
+                    return difficulty # Returns medium difficulty to main
+                elif hard_rect.collidepoint(event.pos): # If hard button pressed
                     difficulty = "hard"
-                    return difficulty
+                    return difficulty # Returns hard difficulty to main
         pygame.display.update()
 
 
-def success(board):
+def success(board): # Checks to see if the user meets the win condition or not
     board.update_board()
-    if board.is_full():
-        if board.check_board():
+    if board.is_full(): # Makes sure board is full
+        if board.check_board(): # Checks if board is correct
             return "win"  # Game won
         else:
             return "loss"  # Game lost
     return None
 
 
-def draw_buttons(screen):
+def draw_buttons(screen): # Reset, restart, and exit buttons that appear below the sudoku board while playing
     button_font = pygame.font.Font(None, 50)
 
     # Initialize text
@@ -106,26 +108,35 @@ def draw_buttons(screen):
     return reset_rect, restart_rect, exit_rect
 
 
-def draw_game_win(screen):
+def draw_game_win(screen): # Displays game win screen
     text = "Game Won!"
 
     game_win_font = pygame.font.Font(None, 100)
+
+    # Color background
     screen.fill(BG_COLOR)
 
+    # initialize and draw title
     game_win_surf = game_win_font.render(text, 0, LINE_COLOR)
     game_win_rect = game_win_surf.get_rect(
         center=(WIDTH // 2, HEIGHT // 2 - 150)
     )
     screen.blit(game_win_surf, game_win_rect)
 
+    # Initialize text
     exit_text = game_win_font.render("Exit", 0, (255, 255, 255))
 
+    #Initialize button background color and text
     exit_surf = pygame.Surface((exit_text.get_size()[0] + 20, exit_text.get_size()[1] + 20))
     exit_surf.fill(LINE_COLOR)
     exit_surf.blit(exit_text, (10, 10))
 
-    exit_rect = exit_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 50))
+    # Initialize button rectangle
+    exit_rect = exit_surf.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 + 50)
+    )
 
+    # Draw button
     screen.blit(exit_surf, exit_rect)
 
     pygame.display.update()
@@ -133,12 +144,15 @@ def draw_game_win(screen):
     return exit_rect
 
 
-def draw_game_over(screen):
+def draw_game_over(screen): # Displays game over screen
+    # Title text
     text = "Game Over :("
-
     game_over_font = pygame.font.Font(None, 100)
+
+    # Color background
     screen.fill(BG_COLOR)
 
+    # initialize and draw title
     game_over_surf = game_over_font.render(text, 0, LINE_COLOR)
     game_over_rect = game_over_surf.get_rect(
         center=(WIDTH // 2, HEIGHT // 2 - 150)
@@ -147,12 +161,17 @@ def draw_game_over(screen):
 
     restart_text = game_over_font.render("Restart", 0, (255, 255, 255))
 
+    # Added button to restart and head back to the main menu
     restart_surf = pygame.Surface((restart_text.get_size()[0] + 20, restart_text.get_size()[1] + 20))
     restart_surf.fill(LINE_COLOR)
     restart_surf.blit(restart_text, (10, 10))
 
-    restart_rect = restart_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 50))
+    # Initialize button rectangle
+    restart_rect = restart_surf.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 + 50)
+    )
 
+    # Draw Button
     screen.blit(restart_surf, restart_rect)
 
     pygame.display.update()
@@ -161,13 +180,13 @@ def draw_game_over(screen):
 
 def key_events(key_event):
     if board.selected_cell:
-        if key_event.key == pygame.K_RETURN:
+        if key_event.key == pygame.K_RETURN: # If the user presses enter, the number will change to a regular value
             if board.selected_cell.sketched_value != 0:
                 board.place_number(board.selected_cell.sketched_value)
-        elif pygame.K_1 <= key_event.key <= pygame.K_9:
+        elif pygame.K_1 <= key_event.key <= pygame.K_9: # Only allows numbers 1 through 9
             sketch_value = key_event.key - pygame.K_0
-            board.sketch(sketch_value)
-        elif key_event.key == pygame.K_BACKSPACE:
+            board.sketch(sketch_value) # Updates the board with a sketched value
+        elif key_event.key == pygame.K_BACKSPACE: # Clears the cell value if backspace is pressed
             board.clear()
 
 
@@ -176,26 +195,26 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Sudoku")
 
-    difficulty = draw_game_start(screen) # Calls function to draw start screen
+    # Calls function to draw start screen and returns difficulty
+    difficulty = draw_game_start(screen)
 
+    # Initializes Board class
     board = Board(WIDTH, 540, screen, difficulty)
-    board.draw()
 
     run = True
-    game_state = None
-
     while run:
-        screen.fill(BG_COLOR)
-        board.draw()
-        reset_rect, restart_rect, exit_rect = draw_buttons(screen)
+        screen.fill(BG_COLOR) # Colors background
+        board.draw() # Draws sudoku board
+        reset_button, restart_button, exit_button = draw_buttons(screen) # Returns coordinates and dimensions of buttons
+
         game_over_button = None
         game_win_button = None
 
-        win_status = success(board)
+        win_status = success(board) # Stores win status
         if win_status == "win":
-            game_win_button = draw_game_win(screen)
+            game_win_button = draw_game_win(screen) # Sent to game win screen
         elif win_status == "loss":
-            game_over_button = draw_game_over(screen)
+            game_over_button = draw_game_over(screen) # Sent to game over screen
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -203,25 +222,25 @@ if __name__ == '__main__':
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = event.pos
-                if reset_rect.collidepoint(x,y):
+                if reset_button.collidepoint(x,y): # On the sudoku screen
                     board.reset_to_original()
-                elif restart_rect.collidepoint(x, y):
+                elif restart_button.collidepoint(x, y): # On the sudoku screen
                     difficulty = draw_game_start(screen)
                     board = Board(WIDTH, 540, screen, difficulty)
-                elif exit_rect.collidepoint(x, y):
+                elif exit_button.collidepoint(x, y): # On the sudoku screen
                     pygame.quit()
                     sys.exit()
-                elif game_over_button is not None and game_over_button.collidepoint(x, y):
+                elif game_over_button is not None and game_over_button.collidepoint(x, y): # On the game over screen
                     difficulty = draw_game_start(screen)
                     board = Board(WIDTH, 540, screen, difficulty)
-                elif game_win_button is not None and game_win_button.collidepoint(x, y):
+                elif game_win_button is not None and game_win_button.collidepoint(x, y): # On the game win screen
                     pygame.quit()
                     sys.exit()
                 else:
-                    clicked_cell = board.click(x, y)
+                    clicked_cell = board.click(x, y) # Stores the clicked cell row and col
                     if clicked_cell:
                         row, col = clicked_cell
-                        board.select(row, col)
+                        board.select(row, col) # Selects the cell that was clicked on
 
             elif event.type == pygame.KEYDOWN:
                 key_events(event)
